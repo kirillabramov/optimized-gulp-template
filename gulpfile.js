da8870696725dacd1caeaa8e2fb28cfe557c0a02
Кирилл Абрамов <work.kirillabramov@gmail.com>
2019-02-07 15:33:25 +0200

    build

--- gulpfile.js
+++ gulpfile.js
@@ -28,7 +28,7 @@ gulp.task('css', async () => {
         .pipe(minifyCSS())
         .pipe(rename('style.css'))
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
-        .pipe(concat('style.min.css'))
+        .pipe(concat('style.css'))
         .pipe(gulp.dest('dist/css'))
 });
 
